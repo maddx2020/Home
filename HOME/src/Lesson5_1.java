e9727bd30708a673737c4cea39439f6d522b6cfa
maddx2020 <madd.xx83@gmail.com>
2020-07-01 02:33:01 +0300

    update

--- HOME/src/Lesson5_1.java
+++ HOME/src/Lesson5_1.java
@@ -2,9 +2,38 @@
 public class Lesson5_1 {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Hello my friend");
 
+		int mass[] = { 3, 10, 9, 4, 4, 2, 5, 8 };
+//		int mass[] = { 9, 3, 8, 7, 4, 8, 10, 8 };
+//		int mass[] = { 9, 1, 8, 7, 4, 8, 5, 8 };
+		int i;
+		int m = mass[0];
+		int n = mass[0];
+		int min = 0;
+		int max = 0;
+		int sum = 0;
+		for (i = 0; i < mass.length; i++) {
+			if (m < mass[i]) {
+				m = mass[i];
+				max = i;
+			}
+			if (n > mass[i]) {
+				n = mass[i];
+				min = i;
+			}
+		}
+		System.out.println("Max value= " + m + " Max index value= " + max);
+		System.out.println("Min value= " + n + " Min index value= " + min);
+		if (max > min) {
+			for (; min + 1 < max; min++) {
+				sum += mass[min + 1];
+			}
+		} else {
+			for (; max + 1 < min; max++) {
+				sum += mass[max + 1];
+			}
+		}
+		System.out.println("Sum value beetwen min and max = " + sum);
 	}
 
 }
